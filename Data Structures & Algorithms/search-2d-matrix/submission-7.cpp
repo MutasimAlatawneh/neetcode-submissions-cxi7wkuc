cd1718c8bdf99990955bdc0978e269db8ac73b8c
motasem alatawna <129550250+MutasimAlatawneh@users.noreply.github.com>
2026-04-14 19:07:46 +0300

    Add: search-2d-matrix - submission-7

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool binary(int target,vector<int>v){
+        int low=0,high=v.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(v[mid]==target)
+            return true;
+            if(v[mid]<target)
+            low=mid+1;
+            else
+            high=mid-1;
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(auto &v:matrix){
+            int n=matrix.size(); 
+            if(binary(target,v))
+            return true;
+        }
+        return false;
+    }
+};
